dc03b5d0065bb42cba26e069171f33c6a911ee13
Руслан Зафаров <zafarovrm@gmail.com>
2021-11-02 14:48:10 +0300

    Добавьте файлы проекта.

---
+++ Task16.2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+
+namespace Task16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = (@"C:\Users\in-user\Desktop\Products.JSON");
+            string currStr = null;
+            int[] array = new int[5];
+            double max = array[0];
+            string name = null;
+            using (StreamReader sr = new StreamReader(path, true))
+                for (int i = 0; i < 5; i++)
+                {
+                    currStr = sr.ReadLine();                    
+                    Product products = JsonSerializer.Deserialize<Product>(currStr);
+                    Console.WriteLine(currStr);
+                    Convert.ToDouble(products.productCost);
+                    if (products.productCost > max)
+                    {
+                        max = products.productCost;
+                        name = products.ProductName;
+                    }
+
+                }            
+            Console.WriteLine("Название самого дорогого товара: {0}", name); 
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        [JsonIgnore]
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public double productCost;
+        public double ProductCost
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    productCost = value;
+                }
+                else
+                {
+                    Console.WriteLine("Цена не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return productCost;
+            }
+        }
+    }
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic)
+            };
+            string path = (@"C:\Users\in-user\Desktop\Products.JSON");
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                Product products = new Product();
+
+                for (int i = 0; i < 5; i++)
+                {                    
+                    Console.WriteLine("Код товара {0}:", i + 1);
+                    products.ProductCode = Convert.ToInt32(Console.ReadLine());                    
+                    Console.WriteLine("Название товара {0}:", i + 1);
+                    products.ProductName = Console.ReadLine();                    
+                    Console.WriteLine("Цена товара {0}:", i + 1);
+                    products.ProductCost = Convert.ToDouble(Console.ReadLine());                    
+                    string json = JsonSerializer.Serialize<Product>(products,option);
+                    sw.WriteLine(json);
+                }
+                Console.ReadKey();
+            }           
+        }
+    }
+    class Product
+    {
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public double productCost;
+        public double ProductCost
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    productCost = value;
+                }
+                else
+                {
+                    Console.WriteLine("Цена не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return productCost;
+            }
+        }
+    }
+}
